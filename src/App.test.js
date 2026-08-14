be0b64d5f7765627bc4824440cd4b1b89e909298
Nusra <nusradotnet@gmail.com>
2026-08-14 11:42:59 +0530

    error Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Modify src/App.test.js to force a failure
 test('renders learn react link', () => {
   expect(1 + 1).toBe(3); // Intentionally failing assertion
 });
